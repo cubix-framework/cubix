ac4ef7ba542ce87596b84dd73bbeae61f04fb0e7
Jakub Kopański <jakub@famisoft.pl>
2025-06-27 15:33:48 +0200

    Tag comment lua funbody replacement

--- src/Cubix/Language/Lua/Parametric/Full/Exclusions.hs
+++ src/Cubix/Language/Lua/Parametric/Full/Exclusions.hs
@@ -15,6 +15,7 @@ import Data.Comp.Trans ( standardExcludedNames )
 import qualified Language.Haskell.TH.Syntax as TH
 import Language.Lua.Annotated.Syntax
 
+-- | TAG_LUA_FUNBODY_EXCEPTION
 -- | We handle FunDef specially because it can have two annotations
 luaExcludedNames :: [TH.Name]
 luaExcludedNames = [''FunBody]
